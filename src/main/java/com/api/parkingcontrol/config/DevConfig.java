a93e1c9767ad03da5a7718f2b312715ff5110578
Mateus Cassio <mateuscassio07@gmail.com>
2023-05-11 10:56:05 -0300

    FEAT:Criando o projeto e os profiles de test e dev com os bancos H2 e Postgres

---
+++ src/main/java/com/api/parkingcontrol/config/DevConfig.java
@@ -0,0 +1,9 @@
+package com.api.parkingcontrol.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+}
